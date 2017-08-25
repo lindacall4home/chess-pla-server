ed148586ad6df7c2f6582fcc60ce8fd8e1bce8f1
Linda Call <lindacall4home@hotmail.com>
2017-08-25 13:41:06 -0600

    setup routing

--- chess-pla-client/src/components/App.js
+++ chess-pla-client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>
+const Home = () => <h2>Home</h2>
+const Register = () => <h2>Register</h2>
+const Meeting = () => <h2>Meeting</h2>
 
 const App = () => {
   return (
     <div>
-      Hi There!
+      <BrowserRouter>
+        <div>
+          <Header/>
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={Register} />
+          <Route path="/meeting" component={Meeting} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
