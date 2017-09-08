fe7a85e50e9e8e272d5d1529adcf2f45dc38c5a2
Linda Call <lindacall4home@hotmail.com>
2017-09-08 11:30:32 -0600

    show winners name

--- chess-pla-client/src/components/meeting/Pairing.js
+++ chess-pla-client/src/components/meeting/Pairing.js
@@ -18,26 +18,22 @@ class Pairing extends Component {
         {this.props.game.white_first_name + " " + this.props.game.white_last_name +"(" + this.props.game.white_player_rank + ")"}
         </td>
         <td>
-          <a
-            id="black-won"
-            onClick={() => this.props.onResultClick(this.props.game, "Black")}
-            className={this.props.game.game_result === "Black" ||
-              this.props.game.game_result === null ? "chess-shown" : "chess-hidden"}
+          <h5
+            className={this.props.game.game_result === null ? "chess-hidden" : " "}
           >
-          </a>
+            {this.props.game.game_result === "Black" ? this.props.game.black_first_name + " " + this.props.game.black_last_name : this.props.game.game_result === "White" ? this.props.game.white_first_name + " " + this.props.game.white_last_name : "Draw"}
+          </h5>
           <a
-            id="white-won"
-            onClick={() => this.props.onResultClick(this.props.game, "White")}
-            className={this.props.game.game_result === "White" ||
-              this.props.game.game_result === null ? "chess-shown" : "chess-hidden"}
+            id="chess-board-btn"
+            className={this.props.game.game_result === null ? "btn" : "chess-hidden"}
+            onClick={() => this.props.onChallengeGameClick(this.props.player)}
           >
-          </a>
-          <a
-            id="draw"
-             onClick={() => this.props.onResultClick(this.props.game, "Draw")}
-             className={this.props.game.game_result === "Draw" ||
-               this.props.game.game_result === null ? "chess-shown" : "chess-hidden"}
-          >
+            <i
+              id="challengeIcon"
+              className="material-icons right"
+            >
+              help
+            </i>
           </a>
         </td>
       </tr>
@@ -45,7 +41,27 @@ class Pairing extends Component {
   }
 }
 
-
+// <a
+//   id="black-won"
+//   onClick={() => this.props.onResultClick(this.props.game, "Black")}
+//   className={this.props.game.game_result === "Black" ||
+//     this.props.game.game_result === null ? "chess-shown" : "chess-hidden"}
+// >
+// </a>
+// <a
+//   id="white-won"
+//   onClick={() => this.props.onResultClick(this.props.game, "White")}
+//   className={this.props.game.game_result === "White" ||
+//     this.props.game.game_result === null ? "chess-shown" : "chess-hidden"}
+// >
+// </a>
+// <a
+//   id="draw"
+//    onClick={() => this.props.onResultClick(this.props.game, "Draw")}
+//    className={this.props.game.game_result === "Draw" ||
+//      this.props.game.game_result === null ? "chess-shown" : "chess-hidden"}
+// >
+// </a>
 function mapStateToProps(state, { game }) {
   return { game };
 }
