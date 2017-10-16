11311c6e64e7c23db8684ea71c684e40d127f5d6
Linda Call <lindacall4home@hotmail.com>
2017-10-16 11:31:11 -0600

    tuned pairing

--- chess-pla-client/src/logic/PairingLogic.js
+++ chess-pla-client/src/logic/PairingLogic.js
@@ -1,39 +1,60 @@
 class PairingLogic{
   createPlayerPairings (meetingPlayers, allGames, meetingId) {
+    console.log('in pairing-players: ', meetingPlayers);
+    console.log('games: ', allGames);
     let pairings = [];
 
     let gamesByBlackPlayer = this.getGamesByPlayer(allGames, "black_player_id");
     let gamesByWhitePlayer = this.getGamesByPlayer(allGames, "white_player_id");
 
+    console.log('games by black player: ', gamesByBlackPlayer);
+    console.log('games by white player: ', gamesByWhitePlayer);
+
     let gamePlayers = meetingPlayers
       .filter(player => player.challenge_game)
       .sort((player1, player2) => player1.current_rank - player2.current_rank);
+
+    console.log('game players: ', gamePlayers);
 
     let board = 1;
     while(gamePlayers.length > 1){
       let player = gamePlayers[0];
 
+      console.log('pairing player: ', player);
+
       let opponentCount = this.getOpponentCount(gamesByWhitePlayer[player.player_id] || [], gamesByBlackPlayer[player.player_id] || [])
 
-      let opponent = gamePlayers[1];
-      let opponentIndex = 1;
-      let playsAgainstOpp = opponentCount[opponent] || 0;
-      let oppColorPref = (gamesByBlackPlayer[player.player_id] || 0 ) -    (gamesByWhitePlayer[player.player_id] || 0);
+      let opponent = null;
+      let opponentIndex = 0;
+      let playsAgainstOpp = 0;
+      let oppColorPref = 0;
 
-      let i = 2;
+      let i = 1;
       while (i < 8 && i < gamePlayers.length){
-        if((opponentCount[gamePlayers[i]] || 0) < playsAgainstOpp){
+        console.log('evaluating opponent: ', gamePlayers[i]);
+        if(opponent === null || (opponentCount[gamePlayers[i].player_id] === undefined ? 0 : opponentCount[gamePlayers[i]]) < playsAgainstOpp){
           opponent = gamePlayers[i];
           opponentIndex = i;
-          playsAgainstOpp = opponentCount[opponent] || 0;
-          oppColorPref = (gamesByBlackPlayer[player.player_id] || 0 ) -    (gamesByWhitePlayer[player.player_id] || 0);
+
+          playsAgainstOpp = opponentCount[opponent.player_id] === undefined ? 0 : opponentCount[opponent.player_id];
+
+          oppColorPref = (gamesByBlackPlayer[player.player_id] === undefined ? 0 : gamesByBlackPlayer[player.player_id].length) -    (gamesByWhitePlayer[player.player_id]  === undefined ? 0 : gamesByWhitePlayer[player.player_id].length || 0);
+
+          console.log('player chosen based on plays');
+          console.log('plays against opponent: ', playsAgainstOpp);
+          console.log('opponent color pref: ', oppColorPref);
         }
-        else if ((opponentCount[gamePlayers[i]] || 0) === playsAgainstOpp){
+        else if ((opponentCount[gamePlayers[i]] === undefined ? 0 : opponentCount[gamePlayers[i].player_id]) === playsAgainstOpp){
+          console.log('plays againt opponent same');
           if(((gamesByBlackPlayer[player.player_id] || 0 ) -        (gamesByWhitePlayer[player.player_id] || 0)) > oppColorPref){
             opponent = gamePlayers[i];
             opponentIndex = i;
-            playsAgainstOpp = opponentCount[opponent] || 0;
+            playsAgainstOpp = opponentCount[opponent.player_id] || 0;
             oppColorPref = (gamesByBlackPlayer[player.player_id] || 0 ) -    (gamesByWhitePlayer[player.player_id] || 0);
+            console.log('player chosen based on color pref');
+            console.log('plays against opponent: ', playsAgainstOpp);
+            console.log('opponent color pref: ', oppColorPref);
+
           }
         }
         i++;
@@ -53,8 +74,10 @@ class PairingLogic{
       }
 
       pairings.push(newGame);
+      console.log('new game: ', newGame);
       gamePlayers.splice(opponentIndex, 1);
       gamePlayers.splice(0, 1);
+      console.log('remaining players: ', gamePlayers);
       board++;
     }
 
@@ -73,6 +96,8 @@ class PairingLogic{
       return acc;
     }, {});
 
+    console.log('white opponents: ', opponents);
+
     opponents = blackGames.reduce((acc, game) => {
       if(acc[game.white_player_id] === undefined){
         acc[game.white_player_id] = 1;
@@ -83,16 +108,18 @@ class PairingLogic{
       return acc;
     }, opponents);
 
+    console.log('all opponents: ', opponents);
+
     return opponents;
   }
 
 
  getGamesByPlayer(games, keyName){
     return games.reduce((acc, game) => {
-      if(acc[keyName] === undefined){
-        acc[keyName] = [];
+      if(acc[game[keyName]] === undefined){
+        acc[game[keyName]] = [];
       }
-      acc[keyName].push(game);
+      acc[game[keyName]].push(game);
       return acc;
     }, {})
   }
